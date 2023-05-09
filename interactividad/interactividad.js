92b21b7f25a3c3390c1245dc76f956c4008ccd24
camus0012 <117209979+camus0012@users.noreply.github.com>
2023-05-09 14:41:34 -0300

    V0.0.8

--- interactividad/interactividad.js
+++ interactividad/interactividad.js
@@ -8,270 +8,59 @@ var firebaseConfig = {
 	appId: "1:914137989689:web:4d42d35387d7332ad103e8"
           };
           
-	    firebase.initializeApp(firebaseConfig);
+          firebase.initializeApp(firebaseConfig);
           
-	    var listaPersonas = [
-		{ dni: "16306958", nombre: "marcelo velardez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-	 	{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-                        { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-      
-	      { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-      
-	      { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-      
-	      { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502413", nombre: "Luisa Lopez" },
-       { dni: "43438262", nombre: "Juan Perez" },
-		{ dni: "22222222", nombre: "Maria Rodriguez" },
-		{ dni: "33333333", nombre: "Pedro Gonzalez" },
-		{ dni: "44444444", nombre: "Ana Martinez" },
-		{ dni: "23502414", nombre: "Luisa Lopez" }
-	      
-
-	      
-
-
-	      
-	    ];
+          var listaPersonas = [
+	{ dni: "", nombre: "LISTA LUCHO BRAVO" },
+	{ dni: "16306958", nombre: "marcelo velardez" },
+	{ dni: "22222222", nombre: "Maria Rodriguez" },
+	{ dni: "33333333", nombre: "Pedro Gonzalez" },
+	{ dni: "44444444", nombre: "Ana Martinez" },
+	{ dni: "", nombre: "LISTA JOSÉ MURUA" },
+	{ dni: "22222222", nombre: "Maria Rodriguez" },
+	{ dni: "33333333", nombre: "Pedro Gonzalez" },
+	{ dni: "44444444", nombre: "Ana Martinez" },
+	{ dni: "23502413", nombre: "Luisa Lopez" },
+	{ dni: "", nombre: "LISTA MARTIN FIERRO" },
+	{ dni: "22222222", nombre: "Maria Rodriguez" },
+	{ dni: "33333333", nombre: "Pedro Gonzalez" },
+	{ dni: "44444444", nombre: "Ana Martinez" },
+	{ dni: "23502414", nombre: "Luisa Lopez" }
+          ];
           
-	    function eliminarPersonas() {
-	      firebase.database().ref('/dnis').once('value', function(snapshot) {
-	        var valoresDB = snapshot.val();
+          function eliminarPersonas() {
+	firebase.database().ref('/dnis').once('value', function(snapshot) {
+	  var valoresDB = snapshot.val();
           
-	        for (var clave in valoresDB) {
-	          var dni = valoresDB[clave].dni;
-	          var index = listaPersonas.findIndex(function(p) {
-		return p.dni === dni;
-	          });
-	          if (index > -1) {
-		listaPersonas.splice(index, 1);
-	          }
-	        }
-          
-	        actualizarLista();
-	      });
+	  for (var clave in valoresDB) {
+	    var dni = valoresDB[clave].dni;
+	    var index = listaPersonas.findIndex(function(p) {
+	      return p.dni === dni;
+	    });
+	    if (index > -1) {
+	      listaPersonas.splice(index, 1);
 	    }
+	  }
           
-	    function actualizarLista() {
-	      var listaHTML = document.getElementById("lista");
-	      listaHTML.innerHTML = "";
+	  actualizarLista();
+	});
+          }
           
-	      listaPersonas.forEach(function(persona) {
-	        var li = document.createElement("li");
-	        li.innerText = persona.dni + " - " + persona.nombre;
-	        listaHTML.appendChild(li);
-	      });
-	    }
-      
+          function actualizarLista() {
+	var listaHTML = document.getElementById("lista");
+	listaHTML.innerHTML = "";
+          
+	listaPersonas.forEach(function(persona) {
+	  var li = document.createElement("li");
+	  li.innerText = persona.dni + " - " + persona.nombre;
+	  if (persona.dni === "") {
+	    li.style.color = "blue";
+	    li.style.marginTop = "10px";
+	    li.style.marginBottom = "10px";
+	  }
+	  listaHTML.appendChild(li);
+	});
+          }
+          
 
 	    
